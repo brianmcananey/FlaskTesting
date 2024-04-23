c163d5ae6422c5a44d2a11586b60f4fe03507861
brianmcananey <138069866+brianmcananey@users.noreply.github.com>
2024-04-23 22:40:37 +0100

    Add files via upload
    needed to create a new repo, as had wrong extensions and last one wouldnt deploy on heroku

---
+++ FlaskTesting/run.py
@@ -0,0 +1,14 @@
+{
+"python.pythonPath": "${FlaskTesting}\\.venv\\Scripts\\python.exe",
+"python.terminal.activateEnvironment": True,
+"python.linting.enabled": True,
+"files.autoSave": "onFocusChange",
+"terminal.integrated.env.windows" : {
+"SECRET_KEY": "my secret key here",
+"DEV": "1",
+"FLASK_DEBUG": "1",
+"MONGO_URI": "my uri here",
+"FLASK_APP": "app.py",
+"FLASK_ENV":"development",
+}
+}
